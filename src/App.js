831e4914adbec705e411822962149b14bf7048de
Sena Cantekin <58258790+senacantekin@users.noreply.github.com>
2023-04-09 05:16:24 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [title, setTitle] = useState("Title");
+  const[descr, setDesc]= useState("Description");
+
+ function submitPost(){
+  const requestBody = {title:title, description:descr, image_url: "image_url"};
+   
+        axios.post('http://localhost:8080/albums', requestBody)
+        .then(response => this.setState({ title: response.data.title , descr:response.data.description}))
+        .catch(error => {
+            
+            console.error('There was an error!', error);
+        });
+
+ }
+ 
+    // Simple POST request with a JSON body using axios
+    
+
+  
+  const [file, setFile] = useState();
+    function handleChange(e) {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+
+
+ 
+  
+
+  useEffect(() => {
+    
+    document.title = title;
+    
+    
+  }, [title,descr]);
+
+  const changeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const changeDescr= (e)=>{
+    setDesc(e.target.value);
+    
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <input 
+        name="title"
+        type="text"
+        onChange={changeTitle}
+       // onFocus={handleFocus}
+        value={title}
+        style={styles.label}/>
+        
+      
+      <div > <input
+      
+      name="descr"
+      type="text"
+      onChange={changeDescr}
+      value={descr}
+      style={styles.input}
+      >
+      </input></div>
+      <h2>Add Image:</h2>
+            <input type="file" onChange={handleChange} />
+            <img src={file}alt="test" />
+      
+
+ 
+      <button
+      onClick={submitPost()}
+      style={styles.button}>
+        submit
+      </button>
+
     </div>
   );
-}
+};
 
 export default App;
+
+// Just some styles
+const styles = {
+  container: {
+    height:600,
+    width: 500,
+    margin: "100px auto",
+    justifyContent: "center",
+  },
+  label: {
+    height:30,
+    width: 200,
+    padding: "10px 10px",
+  },
+  input: {
+    height:300,
+    width: 400,
+    padding: "30px 20px",
+  },
+  button:{
+    
+    width: 100,
+    padding:18,
+    backgroundColor: "green",
+    
+   
+    
+  }
+};
+
---
+++ src/Snx.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+ class Snx extends Component {
+  render() {
+    return (
+      <div className='col-md-8 mb-4'>
+       
+
+
+       
+      </div>
+    )
+  }
+}
+export default Snx;
+
